eeaab25b6b8e453107d1fed1bd9551c7ddc0d559
Berkay Kaya <62906693+bberkay@users.noreply.github.com>
2023-04-02 20:54:25 +0300

    pagination-style

--- src/lib/components/home/Repositories.svelte
+++ src/lib/components/home/Repositories.svelte
@@ -1,20 +1,70 @@
 <script lang="ts">
-    export let data:any;
+    import { pagination } from "$stores/pagination";
+
+    export let data: any;
+
+    let current_pagination = 1;
+    function increment() {
+        if (current_pagination < 3) {
+            current_pagination += 1;
+            pagination.update((n) => n + 1);
+        }
+    }
+
+    function decrease() {
+        if (current_pagination > 1){
+            current_pagination -= 1;
+            pagination.update((n) => n - 1);
+        }
+    }
+
+    function setPagination(n: number) {
+        current_pagination = n;
+        pagination.set(n);
+    }
+
 </script>
 
 {#each data["data"] as repository}
-<div class="card inner-card text-start mb-4">
-    <div class="pt-2 ps-3 pe-3 card-header-section">
-        <h5>{repository.title}<small><a href={repository.link} class = "card-title-link ms-1" target="_blank"><span class="badge text-bg-primary">See</span></a></small></h5>
+    <div class="card inner-card text-start mb-4">
+        <div class="pt-2 ps-3 pe-3 card-header-section">
+            <h5>
+                {repository.title}<small
+                    ><a
+                        href={repository.link}
+                        class="card-title-link ms-1"
+                        target="_blank"
+                        ><span class="badge text-bg-primary">See</span></a
+                    ></small
+                >
+            </h5>
+        </div>
+        <div class="border-div mt-1 mb-1" />
+        <div class="pt-2 ps-3 pe-3 pb-3 card-content">
+            <p class="card-content-p">{repository.description}</p>
+            <small>
+                {#each repository.tags as tag}
+                    <span class="badge bg-secondary me-1">{tag}</span>
+                {/each}
+            </small>
+        </div>
     </div>
-    <div class = "border-div mt-1 mb-1"></div>
-    <div class="pt-2 ps-3 pe-3 pb-3 card-content">
-        <p class = "card-content-p">{repository.description}</p>
-        <small>
-        {#each repository.tags as tag}
-            <span class = "badge bg-secondary me-1">{tag}</span>
-        {/each}
-        </small>
-    </div>
-</div>
 {/each}
+
+<nav aria-label="Page navigation example" class = "mx-auto">
+    <ul class="pagination">
+        <li class="page-item">
+            <button class="page-link bg-svelte" on:click={decrease} aria-label="Previous">
+                <span aria-hidden="true">&laquo;</span>
+            </button>
+        </li>
+        <li class="page-item"><button class="page-link" on:click={() => setPagination(1)}>1</button></li>
+        <li class="page-item"><button class="page-link" on:click={() => setPagination(2)}>2</button></li>
+        <li class="page-item"><button class="page-link" on:click={() => setPagination(3)}>3</button></li>
+        <li class="page-item">
+            <button class="page-link bg-svelte" on:click={increment} aria-label="Next">
+                <span aria-hidden="true">&raquo;</span>
+            </button>
+        </li>
+    </ul>
+</nav>
--- src/lib/components/home/Search.svelte
+++ src/lib/components/home/Search.svelte
@@ -1,39 +1,5 @@
-<script lang="ts">
-    import { pagination } from "$stores/pagination";
-
-    function increment() {
-        pagination.update((n) => n + 1);
-    }
-
-    function decrease() {
-        pagination.update((n) => n - 1);
-    }
-</script>
-
-<p>{pagination}</p>
-<div class="input-group mb-3">
-    <!-- Search and Pagination -->
-    <button class="btn bg-svelte btn-pagination" on:click={decrease}>
-        <span aria-hidden="true">
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                class="bi bi-chevron-double-left"
-                viewBox="0 0 16 16"
-            >
-                <path
-                    fill-rule="evenodd"
-                    d="M8.354 1.646a.5.5 0 0 1 0 .708L2.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"
-                />
-                <path
-                    fill-rule="evenodd"
-                    d="M12.354 1.646a.5.5 0 0 1 0 .708L6.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"
-                />
-            </svg>
-        </span>
-    </button>
+<!-- Search-->
+<div class="mb-3">
     <form method="POST" action="/search" class="text-start">
         <input
             type="text"
@@ -46,26 +12,5 @@
             required
         />
     </form>
-    <button class="btn bg-svelte btn-pagination" on:click={increment}>
-        <span aria-hidden="true">
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                class="bi bi-chevron-double-right"
-                viewBox="0 0 16 16"
-            >
-                <path
-                    fill-rule="evenodd"
-                    d="M3.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L9.293 8 3.646 2.354a.5.5 0 0 1 0-.708z"
-                />
-                <path
-                    fill-rule="evenodd"
-                    d="M7.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L13.293 8 7.646 2.354a.5.5 0 0 1 0-.708z"
-                />
-            </svg>
-        </span>
-    </button>
-    <!-- Search and Paginatino END -->
 </div>
+<!-- Search END -->
