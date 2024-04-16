65149719bbad6f438b74e2d6f15e57f362552950
Berkay Kaya <62906693+bberkay@users.noreply.github.com>
2024-04-16 23:04:58 +0300

    updated-for-vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-import adapter from '@sveltejs/adapter-node';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from '@sveltejs/adapter-vercel';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -12,8 +12,8 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
-		alias:{
-			$db: './src/db' // alias for db folder
+		alias: {
+			$db: './src/db',
 		}
 	}
 };
